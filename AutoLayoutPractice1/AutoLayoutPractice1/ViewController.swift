b3633f4c0e35050e3fa7f6a9558f093b3460a2d8
himatheja <himatheja@divum.in>
2019-05-30 14:35:16 +0530

    AutoLayout pratice 1

---
+++ AutoLayoutPractice1/AutoLayoutPractice1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice1
+//
+//  Created by Divum on 29/05/19.
+//  Copyright © 2019 Divum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup aletr loading the view, typically from a nib.
+    }
+}
+
